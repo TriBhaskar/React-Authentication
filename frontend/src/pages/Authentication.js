e468caebfd56b5f91acddb5674f4d46ee833e0e5
TriBhaskar <panthribhaskar@gmail.com>
2024-02-16 22:12:14 +0530

    Add token to local storage and update API calls

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -49,6 +49,10 @@ export async function action({ request }) {
     throw json({ message: "Authentication failed" }, { status: 500 });
   }
 
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
   // Redirect to the home page on successful authentication
   return redirect("/");
 }
